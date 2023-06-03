efb4ef9e3730b521af1bb006dcb0689d19c3c6d1
fcappon <118693275+fcappon@users.noreply.github.com>
2023-06-03 13:21:08 +0100

    Moved Events and News into Javascript Array
    Events and News are now stored into a JavaScript Array and they are loaded using HTML. The homepage loads the latest 2 events and news articles.

---
+++ loadarticles.js
@@ -0,0 +1,135 @@
+// articles
+
+// event data
+const events =[{
+    title: "yESAO Congress 2023",
+    shortText: "August 28th till 29th in Bergamo, Italy. We are looking forward to meeting you all! We are preparing an exciting program.",
+    longText: "We are excited to announce the upcoming European Society for Artificial Organs (ESAO) conference in Bergamo, Italy, taking place from August 28th to September 1st, 2023. This conference is a significant event in the field of Artificial Organs and will bring together researchers, clinicians, and industry professionals from all over the world to discuss the latest advancements in the field.<br><br> As part of the ESAO conference, the yESAO meeting will take place on August 28th-29th, offering young researchers a platform to present their research and network with peers and senior members in the field.<br><br> The conference program includes keynote lectures, symposia, oral and poster presentations, and exhibition of the latest technological advancements in Artificial Organs. This is an excellent opportunity to learn about cutting-edge research, explore new technologies, and connect with colleagues from different parts of the world.<br><br> We invite all researchers, clinicians, and industry professionals interested in Artificial Organs to participate in this exciting event. Don't miss the chance to expand your knowledge, connect with peers, and enjoy the beautiful city of Bergamo. <br><br> Stay tuned for more updates and registration details. We look forward to welcoming you in Bergamo!<br><br> <button class='button5'><a href='https://www.esao2023.com/' target='_blank'>Website ESAO 2023</a></button>",
+    image:"'https://www.esao.org/wp-content/uploads/2023/01/ESAO_Congress_2023_Bergamo.jpg'",
+    alt:"ESAO and IFAO Congress August 29-September 1,2023 Bergamo Italy",
+},
+{
+    title:"yESAO Webinar 2023",
+    shortText: "Join us for the first ever yESAO webinar on 11.07.2023 from 12 pm to 1 pm (CEST). We will cover some essential tips and tricks for attending and presenting at conferences.",
+    longText: "We are excited to announce the launch of the yESAO webinar series, a new initiative to connect and support young researchers in the field of artificial organs. The first webinar will take place on 11.07.2023 from 12 pm to 1 pm (CEST) and will cover some essential tips and tricks for attending and presenting at conferences. Whether you are new to the symposium scene or a seasoned attendee, you will find valuable insights from our speakers: Martin Maw will share his experience of attending his first conference and what to expect there, Femke Cappon will give some advice on how to prepare your conference talk, and Tom Bode will introduce the yESAO Buddy Programme, a mentoring scheme that pairs up junior and senior members. To join the webinar, please use the button to sign up and we will send you a link to the video conference. We hope to see you there! If you have any questions, please feel free to contact the webinar team.<br><br><button class='button5'><a href='https://docs.google.com/forms/d/e/1FAIpQLScZCT7HrSeH1gVWRrUcKL7YyW3gkENBlHqiIE5aMG1zuRb2ag/viewform' target='_blank'>Sign Up</a></button>",
+    image:"'https://media.licdn.com/dms/image/D4D22AQGlOCe_jZ63sg/feedshare-shrink_1280/0/1685723530278?e=1688601600&v=beta&t=Frna3T02rgXG3s6qi2NiSEQpicmM6oUYYmsHP5kQeog'",
+    alt:"advertisement poster of the yESAo webinar",
+}]
+
+// past event data
+const pastevents =[
+{
+    title:"48th Annual ESAO Congress 2022",
+    longText: "We're happy to report that the ESAO conference in Krems, Austria from September 6th-10th, 2022 was a great success, with the yESAO conference taking place from September 6th-7th as part of the event. Young researchers from all over the world came together to present their work, engage in stimulating discussions, and forge connections with colleagues in the field. The yESAO conference in particular was a highlight, with participants enjoying a variety of informative and thought-provoking talks, as well as opportunities for networking and collaboration. Overall, the conference was a valuable experience for all involved and we look forward to continuing to support the next generation of researchers in the field of artificial organs.",
+    image:"'./images/heroimageabout.jpg'",
+    alt:"Group pictures of participants of the yESAO conference 2022"
+},
+{
+    title:"47th Annual ESAO Congress 2021",
+    longText: "We are grateful to all who participated in the virtual yESAO Congress held in 2021. Despite the challenges posed by the pandemic, it was a memorable event, and we thank everyone who joined us online. We appreciate the commitment and support of all yESAOns who persevered through these difficult times to make this event a success. The yESAO core team extends their heartfelt thanks to each and every one of you for contributing to the virtual Congress and making it a remarkable experience.",
+    image:"'https://www.esao.org/wp-content/uploads/2021/12/Foto_yESAO-768x432.png'",
+    alt:"Zoom group pictures of participants of the yESAO conference"    
+},
+{
+    title:"1st Cloudy-yESAO 2020: A Successful Digital Conference Amidst Unprecedented Times",
+    longText: "We're pleased to announce that this year's yESAO event, the 'Cloudy-yESAO,' was a great success! Due to the circumstances, the event took place in an altered form as a digital online conference from September 7th to 11th, 2020. Despite the changes, the format still included traditional elements, such as scientific sessions and workshops, with novel elements enabled by the online conference concept. We were thrilled to have 100 participants, with 24 scientific presentations, 4 special talks about research during the pandemic, two great keynotes, and one digital workshop. The core team had much fun throughout the event and appreciated the enthusiasm and support of all the participants. Thanks to everyone who participated in making this year's yESAO a great success, and we look forward to continuing the momentum in future events.",
+    image:"'https://www.esao.org/wp-content/uploads/2020/06/yesao_logo_draft002-768x328.png'",
+    alt:"Digital conference, 07-11 September 2021 (coordinators: Ben Torner, Martin Maw)"    
+},
+{
+    title:"46th Annual ESAO Congress 2019",
+    longText: "The Hannover, Germany event held from September 03-07, 2019 was a fantastic success, with Alessandra Molteni and Marc Mueller serving as coordinators, and Michael Bode, Christina Feldmann, and Marc Mueller as local organizers. This event brought together researchers from around the world to discuss the latest advances in artificial organs and tissue engineering. Participants enjoyed a range of engaging scientific sessions and workshops, as well as opportunities to network and share ideas with their peers. The event provided an excellent platform for researchers to exchange knowledge, showcase their work, and collaborate on innovative solutions in the field. We are grateful to the coordinators and local organizers for their efforts in creating such a valuable event for the scientific community.",
+    image:"'https://www.esao.org/wp-content/uploads/2018/11/Announcement_website-500x331.png'",
+    alt:"Hannover Conference details, past event"    
+}
+]
+
+// news data
+const news = 
+[
+{
+title: "The yESAO Exchange Award 2023: Experience International Research and Cooperation",
+ shortText:"The European Society for Artificial Organs (ESAO) has announced the launch of the yESAO Exchange Award 2023, aimed at promoting research collaboration between young researchers of the ESAO community.",
+longText: "The ESAO has announced the launch of the yESAO Exchange Award 2023, aimed at promoting research collaboration between young researchers of the ESAO community. The program offers a unique opportunity for junior members of the ESAO to gain research experience and training in different environments, improve their scientific knowledge and establish international and interdisciplinary cooperation.<br><br> The yESAO Exchange Program is a two-step program where two young researchers from different institutes collaborate on a short project, with each researcher spending at least one week in the host institute of the other researcher. The program offers funding of €750 for each researcher to cover eligible costs associated with the exchange. The program is open only to ESAO junior members interested in improving their scientific knowledge and cooperation.<br><br> The application process for the program is simple and requires a complete application consisting of a cover page, an exchange proposal written by the two researchers involved, a curriculum vitae of the two researchers, a letter of support from the supervisors of the two hosting institutes, and an ESAO junior membership application form (if not already a member). The deadline for applications is February 20th, 2023, and all documents should be submitted to yesao@esao.org.<br><br> The evaluation process will be conducted by three reviewers nominated by the yESAO executive committee, who will evaluate the applications based on the quality of content, novelty of the problem, quality of working plan, and exchange of methods. The two exchange projects with the highest scores will be funded by ESAO.<br><br> After the exchange program, the researchers enrolled in the program will be required to prepare a final report (2 pages) of the exchange experience and submit it to yESAO. They will also be required to make an oral presentation on the experience at the ESAO meeting. Participants are encouraged to submit an abstract to the ESAO conference if relevant scientific results arise from the exchange program.<br><br> The yESAO Exchange Award is an opportunity for young researchers to gain experience, collaborate with other researchers, and establish international and interdisciplinary cooperation. The program will help participants enhance their research skills and provide them with a platform to present their research findings at the main ESAO congress in September 2023.",
+image: "'./images/exchangeaward.jpg'",
+alt: "Pin board with Europe and world map"
+},
+
+{
+title: "yESAO to Host Our First-Ever Webinar on Artificial Organs and Early Career Research Skills",
+shortText: "The European Society for Artificial Organs (ESAO) is excited to announce that we are currently in the process of organising our first-ever webinar.",
+longText: "The European Society for Artificial Organs (ESAO) is excited to announce that we are currently in the process of organising our first-ever webinar. This is a significant milestone for the society and a fantastic opportunity for researchers in the field of artificial organs to come together and share their knowledge and experiences.<br><br> The webinar will focus on a range of topics related to artificial organs research, including new technologies, innovative research projects, and emerging trends in the field. Attendees will have the opportunity to learn from leading experts in the field and engage in lively discussions and debates on the latest research findings.<br><br>At this stage, we are still in the planning phase and have not yet opened registration for the webinar. However, we encourage all members of the ESAO and those interested in artificial organs research to stay tuned for further updates and information.<br><br> We are working hard to ensure that this webinar is a success and a valuable experience for all attendees. Our team is currently finalising details of the programme and speakers, and we will provide further information on the registration process as soon as possible.<br><br>This webinar is just one of the many exciting initiatives that the ESAO is working on to advance the field of artificial organs and promote collaboration among researchers. We are committed to creating opportunities for young researchers to develop their skills and knowledge, and to providing a platform for established experts to share their expertise.<br><br>",
+image:"'./images/webinar.jpg'",
+alt: "young man talking to a camera for a webinar"
+},
+
+{
+title: "The yESAO Exchange Award 2023 Applications Winners",
+shortText: "The two exchange projects with the highest scores have been selected, and the winners will be funded by ESAO. Congratulations to the winners, and we look forward to hearing about their exchange experiences at the main ESAO congress in September 2023. For those who missed out, the call for applications for the yESAO Exchange Award 2024 will open after the conference in 2023. Stay tuned for more updates!",
+longText: "The two exchange projects with the highest scores have been selected, and the winners will be funded by ESAO. Congratulations to the winners, and we look forward to hearing about their exchange experiences at the main ESAO congress in September 2023. For those who missed out, the call for applications for the yESAO Exchange Award 2024 will open after the conference in 2023. Stay tuned for more updates!",
+image:"'./images/exchangeaward.jpg'",
+alt: "Pin board with Europe and world map"
+},
+
+{
+title: "yESAO Conference 2022 Austria, Krems - A Huge Success!",
+shortText: "The European Society for Artificial Organs (ESAO) and the young ESAO researchers group (yESAO) would like to extend our sincerest gratitude to the hosts and participants of the ESAO Conference that took place in Krems, Austria on September 6th-10th, 2022.",
+longText: "The European Society for Artificial Organs (ESAO) and the young ESAO researchers group (yESAO) would like to extend our sincerest gratitude to the hosts and participants of the ESAO Conference that took place in Krems, Austria on September 6th-10th, 2022. <br><br> The conference was a resounding success, with a multitude of experts in the field of artificial organs presenting and sharing their knowledge and research. Attendees were treated to an array of informative talks, engaging discussions, and lively debates, as well as the opportunity to network with fellow researchers and professionals.<br><br> ESAO and yESAO would like to extend a special thank you to the conference hosts and organizers, whose hard work and dedication made this event possible. Without their efforts, this valuable opportunity for education and collaboration would not have been possible.<br><br> Additionally, we would like to thank all of the conference participants for their attendance and contributions. Your active engagement in the conference made for a stimulating and productive event.<br><br> As we reflect on the success of the 2022 ESAO Conference, we look forward to the future of the field of artificial organs and the continued growth and development of ESAO and yESAO. Once again, we extend our gratitude to all who participated and made this event a success.",
+image:"'./images/heroimage.jpg'",
+alt: "A group of researchers of the yESAO posing for a group picture"    
+}
+]
+
+// code to load the articles/events on the correct page
+
+numArticles = news.length;
+numEvents = events.length;
+numPastEvents = pastevents.length;
+
+// create an html formatted array with all news, events and past events
+
+let allNews = new Array(numArticles);
+for (let i = 0; i < numArticles; i ++) {allNews[i] = "<div class='article'> <img src=" + news[i].image + "alt=" + news[i].alt + "/> " + "<h2>"+ news[i].title + "</h2>" + " <p> " + news[i].longText + " </p> </div>";
+}
+
+let allEvents = new Array(numEvents);
+for (let i = 0; i < numEvents; i ++) {allEvents[i] = "<div class='article'> <img src=" + events[i].image + "alt=" + events[i].alt + "/> " + "<h2>"+ events[i].title + "</h2>" + " <p> " + events[i].longText + " </p> </div>";
+}
+
+let allPastEvents = new Array(numPastEvents);
+for (let i = 0; i < numPastEvents; i ++) {allPastEvents[i] = "<div class='article'> <img src=" + pastevents[i].image + "alt=" + pastevents[i].alt + "/> " + "<h2>"+ pastevents[i].title + "</h2>" + " <p> " + pastevents[i].longText + " </p> </div>";
+}
+
+// News and event data on the home screen
+
+var element1 = document.getElementById('latest-news1');
+if (element1 !== null){
+window.onload = function() {
+
+document.getElementById("latest-news1").innerHTML = "<img src="+ news[numArticles-2].image + "class='imageblog'" + "alt="+ news[numArticles-2].alt + "/>" + "<h2>"+ news[numArticles-2].title + "</h2>" + " <p> " + news[numArticles-2].shortText + " </p> "   
+
+document.getElementById("latest-news2").innerHTML = "<h2>"+ news[numArticles-1].title + "</h2>" + " <p> " + news[numArticles-1].shortText + " </p> "
+
+document.getElementById("latest-events1").innerHTML = "<img src="+ events[numEvents-2].image + "class='imageblog'" + "alt="+ events[numEvents-2].alt + "/>" + "<h2>"+ events[numEvents-2].title + "</h2>" + " <p> " + events[numEvents-2].shortText + " </p> "
+
+document.getElementById("latest-events2").innerHTML = "<h2>"+ events[numEvents-1].title + "</h2>" + " <p> " + events[numEvents-1].shortText + " </p> "
+}
+}
+
+// News data on the news page
+var element2 = document.getElementById('all-news');
+if (element2 !== null){
+    window.onload = function() {
+    document.getElementById("all-news").innerHTML = allNews
+    }
+}
+
+// Event data on the event page
+var element3 = document.getElementById('all-events');
+if (element3 !== null){
+    window.onload = function() {
+    document.getElementById("all-events").innerHTML = allEvents
+    document.getElementById("all-pastevents").innerHTML = allPastEvents
+    }
+}
+
--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-/* NAV BAR RESPONSE*/
-
+// NAV BAR RESPONSE
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
@@ -9,31 +8,21 @@ function myFunction() {
     }
   }
 
-
-  /* MEET THE TEAM IMAGE CAROUSEL*/
-
+// MEET THE TEAM IMAGE CAROUSEL
+ var element5 = document.getElementById('team-carousel');
+ if (element5 !== null){
   const slider = document.querySelector('.slider');
   const images = document.querySelectorAll('.slider img');
-  
   const imageCount = images.length;
-  console.log(images)
-  
-
   const imageWidth = 180; // set this to the desired image width
   const padding = 20; // adjust this value as needed
-
-  console.log((imageWidth + padding*2)*imageCount)
-  const sliderMargins = parseInt(getComputedStyle(slider).marginLeft) + parseInt(getComputedStyle(slider).marginRight); // get the sum of the left and right margins of the slider container
-  const visibleWidth = window.innerWidth - sliderMargins; // subtract the slider margins from the viewport width
-  console.log(visibleWidth)
-
-  const totalWidth =  (imageWidth + padding*2) * images.length; // calculate the total width of all the images in a slider
+  const visibleWidth = window.innerWidth; // subtract the slider margins from the viewport width
+  const sliderMargins = visibleWidth*0.2;
+  const totalWidth = (imageWidth + padding) * images.length; // calculate the total width of all the images in a slider
   const translate = (imageWidth + padding*2); // calculate how much each time the images move to the left
- 
-  const maxSlides = Math.floor((totalWidth-1.15*visibleWidth)/translate); // calculate the number of times the images need to move to the left to show all
-  console.log(maxSlides)
-
+  const maxSlides = Math.floor((totalWidth-(visibleWidth-sliderMargins))/translate); // calculate the number of times the images need to move to the left to show all
   let counter = 0;
+
   
   function slide() {
     if (counter === maxSlides) { // check if the max slides have been reached
@@ -41,13 +30,16 @@ function myFunction() {
     } else {
       counter++;
     }
-
     const translateX = -counter * translate; // calculate the translateX value based on the current slide
     slider.style.transform = `translateX(${translateX}px)`; // apply the translateX value to the slider
-    
   }
   
   setInterval(slide, 3000); //Every 3 seconds the images will move
 
-
- 
+  //console.log(images)
+  //console.log((imageWidth + padding*2)*imageCount)
+  //console.log(visibleWidth + " visibleWidth")
+  //console.log(sliderMargins+  " sliderMargins")
+  //console.log(images.length + " image legnth")
+  //console.log(maxSlides +"Maximum slides")
+ }
